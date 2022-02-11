5244f40707186fd90d73eeda7c1fd7af8f2e5d3c
naresh9908 <nareshbashaboina4@gmail.com>
2022-02-11 09:52:55 +0530

    update bookcricket.h

--- 3_Implementation/Inc/bookcricket.h
+++ 3_Implementation/Inc/bookcricket.h
@@ -1,1 +1,18 @@
- 
+ /**
+ * @file bookcricket.h
+ * @author Bashaboina Naresh 
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+ #ifdef __BOOKCRICKET_H__
+#define __BOOKCRICKET_H__
+
+int GetPlayerToss(char player_1[],char player_2[],int oversToplay);
+
+int batting(char battingplayer[],char bowlingplayer[],int oversToplay);
+
+#endif
